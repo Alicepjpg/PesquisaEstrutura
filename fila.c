278085c31d79e74b49805c81fb476a6ed3e2c5ee
Evellyn <evellynn.camposs@gmail.com>
2026-06-20 02:45:24 -0300

    Add files via upload
    Códigos referentes aos tópicos 7.1 e 7.2

---
+++ fila.c
@@ -0,0 +1,136 @@
+
+#include <stdio.h>
+#include <string.h>
+
+#define TAM 10
+
+typedef struct {
+	int numero;
+	char cliente[50];
+} Pedido;
+
+Pedido fila[TAM];
+
+int inicio = 0;
+int fim = -1;
+int quantidade = 0;
+
+int filaVazia() {
+	return quantidade == 0;
+}
+
+void adicionarPedido(int numero, char cliente[]){
+	if (quantidade == TAM) {
+		printf("\nFila de pedidos cheia!\n");
+		return;
+	}
+
+	fim++;
+	fila[fim].numero = numero;
+	strcpy(fila[fim].cliente, cliente);
+
+	quantidade++;
+
+	printf("\nPedido %d de %s adicionado com sucesso!\n",
+	       numero, cliente);
+}
+
+void atenderPedido() {
+	if (filaVazia()) {
+		printf("\nNao existe pedidos na fila.\n");
+		return;
+	}
+
+	printf("\nPedido atendido:\n");
+	printf("Numero: %d\n", fila[inicio].numero);
+	printf("Cliente: %s\n", fila[inicio].cliente);
+
+	inicio++;
+	quantidade--;
+}
+
+void consultarPrimeiroPedido() {
+	if (filaVazia()) {
+		printf("\nFila vazia.\n");
+		return;
+	}
+
+	printf("\nPrimeiro pedido da fila:\n");
+	printf("Numero: %d\n", fila[inicio].numero);
+	printf("Cliente: %s\n", fila[inicio].cliente);
+}
+
+void exibirFila() {
+	if (filaVazia()) {
+		printf("\nFila vazia.\n");
+		return;
+	}
+
+	printf("\n--- FILA DE PEDIDOS ---\n");
+
+	for (int i = inicio; i <= fim; i++) {
+		printf("Pedido %d - Cliente: %s\n",
+		       fila[i].numero,
+		       fila[i].cliente);
+	}
+}
+
+int main() {
+	int opcao;
+	int numeroPedido;
+	char nomeCliente[50];
+
+	do {
+		printf("\n---- SISTEMA DE PEDIDOS ----\n");
+		printf("1 - Adicionar pedido\n");
+		printf("2 - Atender pedido\n");
+		printf("3 - Consultar primeiro pedido\n");
+		printf("4 - Exibir fila de pedidos\n");
+		printf("5 - Verificar fila de pedidos\n");
+		printf("0 - Sair\n");
+		printf("Escolha: ");
+		scanf("%d", &opcao);
+
+		switch(opcao) {
+
+		case 1:
+			printf("Numero do pedido: ");
+			scanf("%d", &numeroPedido);
+
+			printf("Nome do cliente: ");
+			scanf(" %[^\n]", nomeCliente);
+
+			adicionarPedido(numeroPedido, nomeCliente);
+			break;
+
+		case 2:
+			atenderPedido();
+			break;
+
+		case 3:
+			consultarPrimeiroPedido();
+			break;
+
+		case 4:
+			exibirFila();
+			break;
+
+		case 5:
+			if (filaVazia())
+				printf("\nA fila esta vazia.\n");
+			else
+				printf("\nA fila possui %d pedido(s).\n", quantidade);
+			break;
+
+		case 0:
+			printf("\nSistema encerrado\n");
+			break;
+
+		default:
+			printf("\nOpcao invalida!\n");
+		}
+
+	} while(opcao != 0);
+
+	return 0;
+}
---
+++ pilha.c
@@ -0,0 +1,123 @@
+
+#include <stdio.h>
+#include <string.h>
+
+#define TAM 20
+
+typedef struct {
+    char pagina[100];
+} Site;
+
+Site pilha[TAM];
+int topo = -1;
+
+int pilhaVazia() {
+    return topo == -1;
+}
+
+int pilhaCheia() {
+    return topo == TAM - 1;
+}
+
+void visitarPagina(char pagina[]) {
+    if (pilhaCheia()) {
+        printf("\nHistorico cheio!\n");
+        return;
+    }
+
+    topo++;
+    strcpy(pilha[topo].pagina, pagina);
+
+    printf("\nPagina visitada: %s\n", pagina);
+}
+
+void voltarPagina() {
+    if (pilhaVazia()) {
+        printf("\nNao ha paginas para voltar.\n");
+        return;
+    }
+
+    printf("\nVoltando da pagina: %s\n", pilha[topo].pagina);
+    topo--;
+}
+
+void paginaAtual() {
+    if (pilhaVazia()) {
+        printf("\nNenhuma pagina no historico.\n");
+        return;
+    }
+
+    printf("\nPagina atual: %s\n", pilha[topo].pagina);
+}
+
+void exibirHistorico() {
+    if (pilhaVazia()) {
+        printf("\nHistorico vazio.\n");
+        return;
+    }
+
+    printf("\n--- HISTORICO DE NAVEGACAO ---\n");
+
+    for (int i = topo; i >= 0; i--) {
+        printf("%d. %s\n", topo - i + 1, pilha[i].pagina);
+    }
+}
+
+int main() {
+    int opcao;
+    char pagina[100];
+
+    do {
+        printf("\n---- HISTORICO DE NAVEGACAO ----\n");
+        printf("1 - Visitar pagina\n");
+        printf("2 - Voltar pagina\n");
+        printf("3 - Consultar pagina atual\n");
+        printf("4 - Exibir historico\n");
+        printf("5 - Verificar historico\n");
+        printf("0 - Sair\n");
+        printf("Escolha: ");
+        scanf("%d", &opcao);
+        getchar();
+
+        switch(opcao) {
+
+            case 1:
+                printf("Digite o nome da pagina: ");
+                fgets(pagina, sizeof(pagina), stdin);
+
+                pagina[strcspn(pagina, "\n")] = '\0';
+
+                visitarPagina(pagina);
+                break;
+
+            case 2:
+                voltarPagina();
+                break;
+
+            case 3:
+                paginaAtual();
+                break;
+
+            case 4:
+                exibirHistorico();
+                break;
+
+            case 5:
+                if (pilhaVazia())
+                    printf("\nO historico esta vazio.\n");
+                else
+                    printf("\nExistem %d pagina(s) no historico.\n", topo + 1);
+                break;
+
+            case 0:
+                printf("\nNavegador encerrado\n");
+                break;
+
+            default:
+                printf("\nOpcao invalida!\n");
+        }
+
+    } while(opcao != 0);
+
+    return 0;
+}
